c78ce6dbaefa24d4112933a4bcdab847580b848d
Kavdee <judodani10@gmail.com>
2020-09-27 17:37:55 +0200

    Fullnaming

--- UserMaintenance/Entities/User.cs
+++ UserMaintenance/Entities/User.cs
@@ -4,16 +4,10 @@ using System.Security.Cryptography.X509Certificates;
 public class User
 {
     public Guid ID { get; set; } = Guid.NewGuid();
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+
     public string Fullname 
     {
-        get 
-        {
-         return string.Format(
-             "{0} {1}",
-             LastName, FirstName);   
-        } 
+        get; set;
     }
 
 }
