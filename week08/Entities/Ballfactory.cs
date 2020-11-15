5a226d2e025a3733e65118fe33e31e6ce5f0654a
Kavdee <judodani10@gmail.com>
2020-11-15 19:53:32 +0100

    Ballfactory

---
+++ week08/Entities/Ballfactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week08.Entities
+{
+    class Ballfactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+       
+}
